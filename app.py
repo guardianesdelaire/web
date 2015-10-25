38ccce324c6b1b4c9d77d05dbe1c4a375c85ae5f
Gabriel Herrero-Beaumont <herrerobeaumont@gmail.com>
2015-10-25 18:02:59 +0100

    created the different sections and included the supportkit widget

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/participa')
+def participa():
+    return render_template("participa.html")
+
+@app.route('/sobre-nosotros')
+def sobrenosotros():
+    return render_template("sobrenosotros.html")
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
